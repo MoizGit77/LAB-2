83207c400fcd24cfd00ed5fbe4337e3191a8ecc8
Muhammad Moiz <sentencedperson@gmail.com>
2026-02-28 13:24:38 +0500

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,16 @@
+public class Demo{
+	public static void main(String args[]){
+		Student s1= new Student();
+		s1.name="Charlie Kirk";
+		s1.Regno="FA25-BEE-000";
+		s1.CGPA = 2.46;
+
+	System.out.println("Name: "+s1.name);
+	
+	System.out.println("Registration: "+s1.Regno);
+	
+	System.out.println("CGPA: "+s1.CGPA);
+}
+
+}
+
